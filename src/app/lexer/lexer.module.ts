bac662b14ccb5963d69c8a4d76ef72f0327dcb33
Maksims <m.levcenko@gmail.com>
2022-04-04 11:04:50 +0300

    #1: setup scanner component files

---
+++ src/app/lexer/lexer.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScannerComponent } from './scanner.component';
+
+
+
+@NgModule({
+  declarations: [
+    ScannerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LexerModule { }
